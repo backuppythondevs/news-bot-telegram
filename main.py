e5c60954aaee281b87e9de06c4eebc649a018b30
JoseVale99 <lc19160007@oaxaca.tecnm.mx>
2023-03-23 16:44:48 -0600

    add: main bot - init schedule backup API

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from news_bot import BotTelegramNews
 from keys import TOKEN_API_TELEGRAM,API_NEW_TOKEN
+import schedule
+import time
+
+
 
 
 if __name__ == '__main__':
@@ -7,6 +11,14 @@ if __name__ == '__main__':
     bot.esperar_comando("start", bot.start)
     bot.esperar_comando("menu", bot.menu_conf)
     bot.contestar_consulta(bot.getNewMessage)
+    
+    schedule.every(1).hours.do(bot.backupAPI)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
+
+    
    
     
    
